b517c684645b3b83d25f975162763361e0df125a
Gesila <duany222@outlook.com>
2025-09-17 15:36:47 +0800

    remove from cart

--- amazon/data/cart.js
+++ amazon/data/cart.js
@@ -1,4 +1,10 @@
-export const cart = [];
+export let cart = [{
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 3
+}, {
+    productId: "77919bbe-0e56-475b-adde-4f24dfed3a04",
+    quantity: 2
+}];
 
 export function addToCart(productId) {
     const quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
@@ -16,4 +22,14 @@ export function addToCart(productId) {
             quantity
         });
     }
+}
+
+export function removeFromCart(productId) {
+    const newCart = [];
+    cart.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+    });
+    cart = newCart; 
 }
--- amazon/scripts/checkout.js
+++ amazon/scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import * as utilsModule from './utils/money.js';
 
@@ -15,7 +15,7 @@ cart.forEach((cartItem) => {
     }
 
     const html = `
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-container-${product.id}">
             <div class="delivery-date">
                 Delivery date: Tuesday, June 21
             </div>
@@ -38,7 +38,7 @@ cart.forEach((cartItem) => {
                 <span class="update-quantity-link link-primary">
                     Update
                 </span>
-                <span class="delete-quantity-link link-primary">
+                <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${product.id}">
                     Delete
                 </span>
                 </div>
@@ -94,4 +94,18 @@ cart.forEach((cartItem) => {
     cartSummaryHTML += html;
 });
 
-document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+function updateSummary() {
+    document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+}
+
+updateSummary();
+
+document.querySelectorAll('.js-delete-link')
+    .forEach((link) => {
+        link.addEventListener('click', () => {
+            const {productId} = link.dataset;
+            removeFromCart(productId);
+
+            document.querySelector(`.js-cart-item-container-${productId}`).remove();
+        });
+    });
