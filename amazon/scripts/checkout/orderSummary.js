f12ca02f4072721fdb5b4ebd5c79a7507f375107
Gesila <duany222@outlook.com>
2025-09-19 10:50:34 +0800

    generate HTML for payment summary

--- amazon/scripts/checkout/orderSummary.js
+++ amazon/scripts/checkout/orderSummary.js
@@ -1,8 +1,8 @@
 // Named import
 import {cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption} from '../../data/cart.js';
-import {products} from '../../data/products.js';
+import {getProduct} from '../../data/products.js';
 import * as utilsModule from '../utils/money.js';
-import {deliveryOptions} from '../../data/deliveryOptions.js';
+import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
 
 // default import: import only one thing
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
@@ -17,22 +17,11 @@ export function renderOrderSummary() {
     let cartSummaryHTML = ''; 
 
     cart.forEach((cartItem) => {
-        let product; 
-
-        for (let i = 0; i < products.length; ++i) {
-            if (products[i].id === cartItem.productId) {
-                product = products[i];
-                break;
-            }
-        }
+        const product = getProduct(cartItem.productId);
 
         const deliveryOptionId = cartItem.deliveryOptionId;
-        let deliveryOption;
-        deliveryOptions.forEach((option) => {
-            if (option.id === deliveryOptionId) {
-                deliveryOption = option; 
-            }
-        });
+        const deliveryOption = getDeliveryOption(deliveryOptionId);
+
         const today = dayjs();
         const deliveryDate = today.add(
             deliveryOption.deliveryDays, 
