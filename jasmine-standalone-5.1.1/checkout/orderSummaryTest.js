941af5647c93d4afcd9072a27fe5023550fbe389
Gesila <duany222@outlook.com>
2025-09-24 11:03:37 +0800

    more test cases and hook used

--- jasmine-standalone-5.1.1/checkout/orderSummaryTest.js
+++ jasmine-standalone-5.1.1/checkout/orderSummaryTest.js
@@ -1,13 +1,17 @@
 import {renderOrderSummary} from '../../amazon/scripts/checkout/orderSummary.js';
-import {loadFromStorage} from '../../amazon/data/cart.js';
+import {loadFromStorage, cart} from '../../amazon/data/cart.js';
 import * as CheckoutHeader from '../../amazon/scripts/checkout/checkoutHeader.js';
 
 describe('test suite: renderOrderSummary', () => {
-    it('displays the cart', () => {
-        document.querySelector('.js-test-container').innerHTML = `<div class="js-order-summary"></div>`;
+    // for the suite
+    const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'; 
+    const productId2 = '54e0eccd-8f36-462b-b68a-8182611d9add'; 
 
-        const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'; 
-        const productId2 = '54e0eccd-8f36-462b-b68a-8182611d9add'; 
+    beforeEach(() => {
+        document.querySelector('.js-test-container').innerHTML = `
+        <div class="js-order-summary"></div>
+        <div class="js-checkout-header"></div>
+        <div class=".js-payment-summary"></div> `;
 
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([{
@@ -24,7 +28,15 @@ describe('test suite: renderOrderSummary', () => {
         spyOn(localStorage, 'setItem');
         loadFromStorage();
         renderOrderSummary();
+    });
 
+    afterEach(() => {
+        // remove after test, clear html
+        document.querySelector('.js-test-container').innerHTML = '';
+    });
+
+    it('displays the cart', () => {
+        
         // has two types of items in the cart
         expect(
             document.querySelectorAll('.cart-item-container').length
@@ -37,5 +49,25 @@ describe('test suite: renderOrderSummary', () => {
         expect(
             document.querySelector(`.js-quantity-label-${productId2}`).innerText
         ).toContain('1'); 
+
+    });
+
+    it('removes a product', () => {
+
+        // note there is not such a class, just for demo purpose
+        // document.querySelector(`.js-delete-link-${productId1}`).click(); 
+        expect(
+            document.querySelectorAll(`.cart-item-container`).length
+        ).toEqual(2); // didn't delete 
+
+        // expect(
+        //     document.querySelector(`.js-cart-item-container-${productId1}`)
+        // ).toEqual(null);
+        expect(
+            document.querySelector(`.js-cart-item-container-${productId2}`)
+        ).not.toEqual(null);
+
+        expect(cart.length).toEqual(2); 
+        expect(cart[0].productId).toEqual(productId1);
     });
 })
