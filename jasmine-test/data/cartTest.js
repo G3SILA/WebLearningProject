2d054f46382ddadcdec45c151627bbeaee6cd6e3
Gesila <duany222@outlook.com>
2025-09-24 13:49:38 +0800

    more tests

--- jasmine-test/data/cartTest.js
+++ jasmine-test/data/cartTest.js
@@ -1,13 +1,17 @@
-import {addToCart, cart, loadFromStorage} from '../../amazon/data/cart.js';
+import {addToCart, cart, loadFromStorage, removeFromCart} from '../../amazon/data/cart.js';
 
 describe('test suite: addToCart', () => {
+
+    beforeEach(() => {
+        spyOn(localStorage, 'setItem');
+    });
+
     it('adds a new product to the cart', () => {
         // mock the original localStorage.getItem
         // use a **self-defined** version for testing only
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([]);
         });
-        spyOn(localStorage, 'setItem');
 
         // mock input object
         spyOn(document, 'querySelector').and.callFake(() => {return { value: "1" };}); 
@@ -20,6 +24,13 @@ describe('test suite: addToCart', () => {
         expect(localStorage.setItem).toHaveBeenCalledTimes(1); 
         expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
         expect(cart[0].quantity).toEqual(1);
+
+        // if localStorage.setItem has been called with the parameters 
+        expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 1,
+            deliveryOptionId: '1'
+        }]));
     });
 
     it('add existing product to the cart', () => {
@@ -30,7 +41,6 @@ describe('test suite: addToCart', () => {
                 deliveryOptionId: '1'
             }]);
         });
-        spyOn(localStorage, 'setItem');
         loadFromStorage();
         spyOn(document, 'querySelector').and.callFake(() => {return { value: "1" };}); 
 
@@ -42,3 +52,39 @@ describe('test suite: addToCart', () => {
         expect(cart[0].quantity).toEqual(2);
     });
 });
+
+describe('test suite: removeFromCart', () => {
+    const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+
+    beforeEach(() => {
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([{
+                productId: productId1,
+                quantity: 1, 
+                deliveryOptionId: '1'
+            }]);
+        });
+        spyOn(localStorage, 'setItem');
+        loadFromStorage();
+    });
+
+    it('remove exist id in the cart', () => {
+        removeFromCart(productId1); 
+        expect(cart.length).toEqual(0);
+        expect(
+            document.querySelector(`.js-cart-item-container-${productId1}`)
+        ).toEqual(null);
+    }); 
+
+    it('remove non-exist id in the cart', () => {
+        expect(cart.length).toEqual(1);
+        removeFromCart('1');
+        expect(cart.length).toEqual(1);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1); 
+        expect(localStorage.setItem).toHaveBeenCalledWith('cart',JSON.stringify([{
+            productId: productId1,
+            quantity: 1, 
+            deliveryOptionId: '1'
+        }]));
+    });
+}); 
--- jasmine-test/utils/moneyTest.js
+++ jasmine-test/utils/moneyTest.js
@@ -14,4 +14,12 @@ describe('test suite: formatCurrency', () => {
     it('rounds up to the nearest cent', () => {
         expect(formatCurrency(2000.5)).toEqual('20.01');
     });
+
+    it ('rounds down', () => {
+        expect(formatCurrency(2000.4)).toEqual('20.00');
+    });
+
+    it ('negative', () => {
+        expect(formatCurrency(-1000)).toEqual('-10.00');
+    });
 });
