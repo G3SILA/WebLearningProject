51a7343a9210d4d354d263e9132c4e8840e194b0
Gesila <duany222@outlook.com>
2025-10-10 17:08:01 +0800

    Use promises to wait code to finish

--- amazon/scripts/checkout.js
+++ amazon/scripts/checkout.js
@@ -6,8 +6,64 @@ import '../data/cart-oop.js';
 // import '../data/cart-class.js';
 import {loadProducts} from '../data/products.js';
 
-loadProducts(() => {
+
+//!!! Note: Promise creates an independent sequence of "next"
+//      so that many promises can run at the same time
+
+// similar to done in jasmine
+// only proceed to the next step when resolve() is called
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve();
+    });
+}).then (() => {
     renderOrderSummary();
     renderPaymentSummary();
 });
 
+
+
+
+// how Promise flatten our code - reduce callbacks
+
+/*
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value1');         // give resolve a value! 
+        });
+    
+    }).then ((value) => {              // parameter named value
+        return new Promise((resolve) => {
+            loadCart(() => {
+                resolve();
+            });
+        });
+    
+    }).then (() => {
+        renderOrderSummary();
+        renderPaymentSummary();
+    });
+*/
+
+
+// Run multiple Promises at a time. Wait for all promises to finish
+// array of promises
+/* 
+    Promise.all([
+        new Promise((resolve) => {
+            loadProducts(() => {
+                resolve();
+            });
+        }), 
+        new Promise((resolve) => {
+            loadCart(() => {
+                resolve();
+            });
+        })
+
+        ]).then((values) => {                // values is an array of values
+            renderOrderSummary();
+            renderPaymentSummary();
+        });
+
+*/ 
